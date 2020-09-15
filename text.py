fbe48e08edeefbc54327e2ef79aaeb5d4cfdda07
Enzo Barrett <enzobarrett@gmail.com>
2020-09-14 23:22:35 -0600

    Add comments to Text class

--- text.py
+++ text.py
@@ -5,6 +5,7 @@ class Text:
         self.win = win
         self.yCounter = 0
 
+    # prints the given string to the screen and moves the cursor down one line
     def addstr(self, string):
         self.win.move(self.y + self.yCounter, self.x)
         self.win.addstr(string)
