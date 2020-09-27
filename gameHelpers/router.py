48ec81414f273c0aaf3fae52887b82e321684375
Enzo Barrett <enzobarrett@gmail.com>
2020-09-27 01:21:23 -0600

    Add gamemode logic and gamemode menu to router.py
    * Gamemode 0 is the gamemode selection menu, gamemode 1 is gameplay (for now)
    * MenuController is initialized with a gamemode selection Menu in __init__
    * If in gamemode 0, Router will pass the character to menuController

--- gameHelpers/router.py
+++ gameHelpers/router.py
@@ -1,12 +1,30 @@
 import curses
 
+from gameHelpers.controllers.menu_controller import MenuController
 from gameHelpers.menu import Menu
+
 
 class Router:
     def __init__(self, config):
+        # 0 is single player, 1 is multi-player
+        self.gameMode = None
         self.state = 0
         self.initialMenu = Menu(config, "Single Player", "Multiplayer")
+        self.menuController = MenuController(self.initialMenu)
 
     def route(self, c):
         if c == curses.KEY_ENTER or c == 10 or c == 13:
+            # before changing to gameplay, store gamemode from menu
+            if self.state == 0:
+                self.gameMode = self.menuController.menu.currIndex
             self.state += 1
+
+        # if true, we are in the gamemode selection menu
+        if self.state == 0:
+            self.menuController.handle_input(c)
+            self.menuController.render()
+
+        # we are in gameplay
+        # TODO: execute a method here
+        if self.state == 1:
+            pass
