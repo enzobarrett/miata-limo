dcbcf95c6b3f1fd96745589ff30c4fc592d5151e
Enzo Barrett <enzobarrett@gmail.com>
2020-09-30 23:37:48 -0600

    Implement GameController in Router
    * GameController is initialized in the constructor
    * If the router is in the gameplay state, GameController.render() is called, drawing the city grid (for now)

--- gameHelpers/router.py
+++ gameHelpers/router.py
@@ -1,16 +1,19 @@
 import curses
 
 from gameHelpers.controllers.menu_controller import MenuController
+from gameHelpers.controllers.game_controller import GameController
 from gameHelpers.menu import Menu
 
 
 class Router:
     def __init__(self, config):
         # 0 is single player, 1 is multi-player
+        self.config = config
         self.gameMode = None
         self.state = 0
-        self.initialMenu = Menu(config, "Single Player", "Multiplayer")
+        self.initialMenu = Menu(self.config, "Single Player", "Multiplayer")
         self.menuController = MenuController(self.initialMenu)
+        self.gameController = GameController(self.config)
 
     def route(self, c):
         if c == curses.KEY_ENTER or c == 10 or c == 13:
@@ -24,7 +27,6 @@ class Router:
             self.menuController.handle_input(c)
             self.menuController.render()
 
-        # we are in gameplay
-        # TODO: execute a method here
+        # we are in gameplay mode, for now just singleplayer
         if self.state == 1:
-            pass
+            self.gameController.render()
