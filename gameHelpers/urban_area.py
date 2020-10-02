e35a59ffd258acbe62a6473dbe79e6673548b5c6
Enzo Barrett <enzobarrett@gmail.com>
2020-10-01 23:50:08 -0600

    Update arguments and usage of UrbanArea
    * Now takes two position arguments: x and y
    * Usage is changed accordingly
    * The blank character is now " " instead of "`"

--- gameHelpers/urban_area.py
+++ gameHelpers/urban_area.py
@@ -1,20 +1,23 @@
 class UrbanArea:
-    def __init__(self, config):
+    def __init__(self, config, y, x):
         self.config = config
 
         # width of area should be half - 1 for a separation line
         self.width = int((self.config.width / 2) - 1)
 
+        # starting position of city grid
+        self.yPos = y
+        self.xPos = x
+
     # draws a simple city grid
     # TODO: give options for different types of city grids
     def render(self):
-        for x in range(7):
-            # TODO: make x a argument for the constructor
-            self.config.win.move(17+x, 0)
+        for yOffset in range(7):
+            self.config.win.move(self.yPos+yOffset, self.xPos)
 
             # if we are on a even line, it is a street
-            if x % 2 == 0:
+            if yOffset % 2 == 0:
                 self.config.win.addstr(33 * '-' + "+---+---+---+---")
-            elif x % 2 == 1:
-                self.config.win.addstr(('`' * 33) + "|```|```|```|```")
+            elif yOffset % 2 == 1:
+                self.config.win.addstr((' ' * 33) + "|   |   |   |   ")
         self.config.win.refresh()
