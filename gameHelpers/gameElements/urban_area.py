6f256fff396a04009d4c480613110eef50ba6368
Enzo Barrett <enzobarrett@gmail.com>
2020-10-03 00:10:15 -0600

    Implement UrbanArea reflection
    * If the urban area is on the right side of the map it will be reflected

--- gameHelpers/gameElements/urban_area.py
+++ gameHelpers/gameElements/urban_area.py
@@ -12,12 +12,26 @@ class UrbanArea:
     # draws a simple city grid
     # TODO: give options for different types of city grids
     def render(self):
-        for yOffset in range(7):
-            self.config.win.move(self.yPos+yOffset, self.xPos)
+        reflect = False
 
-            # if we are on a even line, it is a street
-            if yOffset % 2 == 0:
-                self.config.win.addstr(33 * '-' + "+---+---+---+---")
-            elif yOffset % 2 == 1:
-                self.config.win.addstr((' ' * 33) + "|   |   |   |   ")
+        # if on the right side of the board, reflect the city grid
+        if self.xPos > self.width:
+            reflect = True
+
+        for yOffset in range(7):
+            self.config.win.move(self.yPos + yOffset, self.xPos)
+
+            if reflect:
+                # if we are on a even line, it is a street
+                if yOffset % 2 == 0:
+                    self.config.win.addstr("---+---+---+---+" + 33 * '-')
+                elif yOffset % 2 == 1:
+                    self.config.win.addstr("   |   |   |   |" + (' ' * 33))
+            else:
+                # if we are on a even line, it is a street
+                if yOffset % 2 == 0:
+                    self.config.win.addstr(33 * '-' + "+---+---+---+---")
+                elif yOffset % 2 == 1:
+                    self.config.win.addstr((' ' * 33) + "|   |   |   |   ")
+
         self.config.win.refresh()
